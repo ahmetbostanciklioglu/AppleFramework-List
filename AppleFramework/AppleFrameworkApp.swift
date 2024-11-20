fe4ae4a2504c84cbaf3aad17b9e9487fd6c93700
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2024-11-20 20:17:01 +0300

    Initial Commit

---
+++ AppleFramework/AppleFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworkApp.swift
+//  AppleFramework
+//
+//  Created by Ahmet Bostancıklıoğlu on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
